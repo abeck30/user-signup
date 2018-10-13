d0547ce7093d8c88a7e00de15df3db888cf10882
Ashton Beck <ashton.l.beck@gmail.com>
2018-10-12 21:38:53 -0500

    working to determine issue

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template,logging
 import cgi
 import os
 import jinja2
 import string
 
+
 template_dir = os.path.join (os.path.dirname (__file__), 'templates')
-jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,9 +14,26 @@ app.config['DEBUG'] = True
 @app.route ('/')
 def index():
     template = jinja_env.get_template('index.html')
-    return template.render()
+    return template.render()  
 
-@app.route ('/missinginfo', methods = ["POST"])
+def isvalidemail(email):
+    if len(email) > 7:
+        if email.isspace():
+            return True
+
+            if "@" and "." in email:
+                return False
+            else:
+                return True  
+
+def isvalidusername(username):
+    username=str(username)
+    if username.isspace():
+        return True
+    else:   
+        return False 
+
+@app.route ('/info', methods=['POST'])
 def missinginfo():
     username = request.form['username']
     username = str(username)
@@ -32,31 +50,48 @@ def missinginfo():
     vpword = len(vpword)
     vpword_error = " " 
 
+    email = request.form['email']
+    email= str(email)
+    email_error = " " 
+
+    app.logger.info("username %s", isvalidusername(username))
+
+    if isvalidusername(username) == True: 
+        username_error = "Username cannot include spaces please select a new username"
+        username ="" 
 
     if username > 20 or username < 3: 
-        username_error = "Username must be between 4 and 19 characters."
-        username =""
+        username_error = "Username cannot be empty or less than 4 characters (max 20 characters)"
+        username =""   
     
     if pword > 20 or pword < 3: 
-        pword_error = "Password must be between 4 and 19 characters."
-        pword = "" 
+        pword_error = "Password cannot be empty or less than 4 characters (max 20 characters)"
+        pword = ""   
+
+    if vpword > 20 or vpword < 3: 
+        vpword_error = "Verified password cannot be empty or less than 4 characters (max 20 characters)"
+        vpword = ""       
 
     if vpword != pword:
         vpword_error = "Password does not match. Try again."
-        vpword =""
-    
-    if not username_error and not pword_error and not vpword_error:
-        username = str(username)
-        return redirect('/welcome?username={0}'.format(username)) 
-    else:
-        template = jinja_env.get_template('index.html')
-        return template.render(username_error=username_error, pword_error=pword_error, vpword_error=vpword_error)
+        vpword =""  
 
-@app.route('/welcome')
-def welcome ():
-    username = request.form['username']
+    if isvalidemail(email) == True:
+        email_error = "Sorry your did not enter a valid email address please try again."
+        email = ""     
+        
+    if not username_error and not pword_error and not vpword_error and not email_error:
+        username = request.form['username']
+        return redirect ('/welcome.html'.format(username))
+    else: 
+        template = jinja_env.get_template('index.html')
+        return template.render(username_error=username_error, pword_error=pword_error, vpword_error=vpword_error,email_error=email_error)
+
+@app.route ('/welcome') 
+def welcome():
+    username=request.form['welcome']
     template = jinja_env.get_template('welcome.html')
-    return template.render(username)
-       
+    return template.render(username=username)
+ 
 
 app.run() 
