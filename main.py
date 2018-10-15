02ab826e18863036738d41bd6b411b36d0e25928
Ashton Beck <ashton.l.beck@gmail.com>
2018-10-14 21:57:45 -0500

    rewrote main code to try to fix errors, still unable to redirect. new error with validating email

--- main.py
+++ main.py
@@ -1,71 +1,53 @@
-from flask import Flask, request, redirect, render_template,logging
+from flask import Flask, request, redirect, render_template
 import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route ('/')
-def index():
-    template = jinja_env.get_template('index.html')
-    return template.render()  
+@app.route('/')
+def index ():
+    return render_template("userinfo.html")
 
-@app.route ('/welcome.html') 
-def welcome():
-    username = request.form['username']
-    template = jinja_env.get_template('welcome.html')
-    return template.render(name=username)
+@app.route('/', methods=['POST'])
+def validate_userinfo():
+    username = request.form['form_name']
+    password = request.form ['pass_word']
+    vpassword = request.form['vp_word']
+    useremail = request.form['useremail']
 
-@app.route ('/userinfo', methods=['POST'])
-def info():
-    username = request.form['username']
-    pword = request.form['pword']
-    vpword = request.form['vpword']
-    email = request.form['email']
+    form_name_error = " "
+    pass_word_error = " "
+    vp_word_error = " "
+    useremail_error = " "
+   
 
-    username = str(username)
-    pword = str(pword)
-    vpword = str(vpword)
-    email= str(email)
+    if len(username) > 20 or len(username) < 3:
+        form_name_error = "Username must be greater than 3 characters and less than 20 characters"
+        username = ''
 
-    username_error = " " 
-    pword_error = " " 
-    vpword_error = " " 
-    email_error = " " 
-
-    if len(username) > 20 or len(username) < 3: 
-        username_error = "Username cannot be empty or less than 4 characters (max 20 characters)"
-        error = username_error
-        username =""   
+    if len(password) > 20 or len(password) < 3:
+        pass_word_error = "Password must be longer than 3 characters and less than 20 "
+        password = ''
     
-    if len(pword) > 20 or len(pword) < 3: 
-        pword_error = "Password cannot be empty or less than 4 characters (max 20 characters)"
-        error = pword_error
-        pword = ""   
+    if vpassword != password:
+        vp_word_error = "Password verification does not match Password"
+        vpassword = ''
 
-    if len(vpword) > 20 or len(vpword) < 3: 
-        vpword_error = "Verified password cannot be empty or less than 4 characters (max 20 characters)"
-        error = vpword_error
-        vpword = ""       
-
-    if vpword != pword:
-        vpword_error = "Password does not match. Try again."
-        vpword =""  
-
-    if len(email)>0 and len(email)<7:
-        email_error = "Sorry your did not enter a valid email address please try again."
-        error = email_error
-        email = ""     
-        
-    if not error:
-        username = request.form['username']
-        return redirect ('/welcome.html')
-    else: 
-        template = jinja_env.get_template('userinfo.html') 
-        return template.render(username_error=username_error, pword_error=pword_error, vpword_error=vpword_error,email_error=email_error)
+    if len(useremail) > 0  and len(username) < 7:
+        useremail_error = "Not a valid email" 
+        useremail = ''   
+       
+    if not form_name_error and not pass_word_error and not vp_word_error and not useremail_error:
+        username = ('form_name')
+        return redirect ('/valid-userinfo'.format(username))
+    else:
+        return render_template('userinfo.html', form_name_error=form_name_error, pass_word_error=pass_word_error, vp_word_error=vp_word_error, useremail_error=useremail_error, username=username, password=password,vpword=vpassword,useremail=useremail)   
 
 
-app.run() 
+@app.route('/valid-userinfo')
+def welcome():
+    form_name = request.form['form_name']
+    return render_template('userwelcome.html', form_name=form_name)
+
+app.run()
