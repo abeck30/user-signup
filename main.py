8a1680157964219b4dc0f8c9bba0ec30a0e6b18a
Ashton Beck <ashton.l.beck@gmail.com>
2018-10-15 19:09:57 -0500

    added additional error codes

--- main.py
+++ main.py
@@ -25,21 +25,37 @@ def validate_userinfo():
     if len(username) > 20 or len(username) < 3:
         form_name_error = "Username must be greater than 3 characters and less than 20 characters"
         username = ''
+    elif " " in username:
+        form_name_error = "Username cannot contain spaces"
+        username = ''
 
     if len(password) > 20 or len(password) < 3:
         pass_word_error = "Password must be longer than 3 characters and less than 20 "
         password = ''
+    elif " " in password:
+        pass_word_error = "Password connot contain spaces "
+        password = ''
     
-    if vpassword != password:
+    if len(vpassword) > 20 or len(vpassword) < 3:
+        vp_word_error = "Password must be longer than 3 characters and less than 20"
+        vpassword = ''
+    elif vpassword != password:
         vp_word_error = "Password verification does not match Password"
         vpassword = ''
 
     if len(useremail) > 0  and len(username) < 7:
         useremail_error = "Not a valid email" 
         useremail = ''   
+        if " " in useremail:
+            useremail_error = "Not a valid email" 
+            useremail = ''
+        elif "@" and "." not in useremail:
+            useremail_error = "Not a valid email" 
+            useremail = '' 
+
        
     if not form_name_error and not pass_word_error and not vp_word_error and not useremail_error:
-        return render_template('userwelcome.html')
+        return render_template('userwelcome.html', username=username )
     else:
         return render_template('userinfo.html', form_name_error=form_name_error, pass_word_error=pass_word_error, vp_word_error=vp_word_error, useremail_error=useremail_error, username=username, password=password,vpword=vpassword,useremail=useremail)   
 
